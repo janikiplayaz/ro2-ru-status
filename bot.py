90f11b4cf1ace46cb7d942e734efe1906940a09d
janikiplayaz <73994734+janikiplayaz@users.noreply.github.com>
2020-11-05 18:04:03 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,143 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import steam
+from steam import game_servers
+from steam import game_servers as gs
+import os
+
+
+Bot = commands.Bot(command_prefix= "!")
+ADDR = r"\gameaddr\37.230.228.193:7778"
+
+COLORS = (
+    15158332,
+    15159868,
+    15095867,
+    15097403,
+    15098939,
+    15034938,
+    15036218,
+    15037754,
+    14973754,
+    14975289,
+    14976825,
+    14912825,
+    14914360,
+    14915896,
+    14851896,
+    14853431,
+    14854711,
+    14790711,
+    14792247,
+    14793782,
+    14729782,
+    14731318,
+    14667317,
+    14668597,
+    14670133,
+    14605877,
+    14147124,
+    13753908,
+    13294900,
+    12901683,
+    12442931,
+    12049459,
+    11590707,
+    11197234,
+    10738482,
+    10345266,
+    9886258,
+    9493041,
+    9034289,
+    8640817,
+    8182065,
+    7788592,
+    7329840,
+    6936624,
+    6543152,
+    6084399,
+    5690927,
+    5232175,
+    4838959,
+    4445486,
+    3986734,
+    3593262,
+    3200046,
+    3003441,
+    3003191,
+    3003197,
+    3002947,
+    3002953,
+    3002703,
+    3002453,
+    3002202,
+    3001952,
+    3067494,
+    3067243,
+    3066993,
+)
+
+
+@Bot.command(aliases=['с'])
+async def статус(ctx):
+    try:
+        server = next(gs.query_master(ADDR))
+        if not (info := gs.a2s_info(server)):
+            raise RuntimeError
+
+        embed_dict = {
+            "title": f'Статус __{info["name"]}__',
+            "fields": [
+                {"name": "Карта:", "value": info["map"], "inline": True},
+                {
+                    "name": "Игроки:",
+                    "value": f"{info['players']}/{info['max_players']}",
+                    "inline": True,
+                },
+            ],
+            "color": COLORS[min(info["players"], 64)],  # 64 is max players
+        }
+    except RuntimeError:
+        embed_dict = {
+            "title": "Произошла ошибка при получении данных о сервере",
+            "description": "Пожалуйста, попробуйте ещё раз позднее.",
+        }
+    finally:
+        embed = discord.Embed().from_dict(embed_dict)
+        await ctx.channel.send(embed=embed)
+
+@Bot.command(aliases=['s'])
+async def status(ctx):
+    try:
+        server = next(gs.query_master(ADDR))
+        if not (info := gs.a2s_info(server)):
+            raise RuntimeError
+
+        embed_dict = {
+            "title": f'Status of __{info["name"]}__ ',
+            "fields": [
+                {"name": "Map:", "value": info["map"], "inline": True},
+                {
+                    "name": "Players:",
+                    "value": f"{info['players']}/{info['max_players']}",
+                    "inline": True,
+                },
+            ],
+            "color": COLORS[min(info["players"], 64)],  # 64 is max players
+        }
+    except RuntimeError:
+        embed_dict = {
+            "title": "An error occurred while retrieving server information",
+            "description": "Please try again later.",
+        }
+    finally:
+        embed = discord.Embed().from_dict(embed_dict)
+        await ctx.channel.send(embed=embed)
+
+
+@Bot.event
+async def on_ready():
+    print("logged in.")
+
+TOKEN = os.environ.get('TOKEN')
